3d42f79b75b411fb4bc827d6ad025ba52e1d00ce
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2020-09-17 16:30:38 +0900

    Created basic components configured with react router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,22 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+import { Router, Route, IndexRoute, BrowserHistory } from 'react-router';
+
+const router = (
+  <Router history={BrowserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.querySelector('#root'));
+
